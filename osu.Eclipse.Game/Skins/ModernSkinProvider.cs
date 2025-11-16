c5755e032d8c420032d8fd6f1c5a6c646da83a16
grahmr <apedrialva@ymail.com>
2025-11-15 20:40:51 -0600

    Add ModernSkinProvider class for skin support

---
+++ osu.Eclipse.Game/Skins/ModernSkinProvider.cs
@@ -0,0 +1,12 @@
+namespace osu.Eclipse.Game.Skins
+{
+    public class ModernSkinProvider : ISkinProvider
+    {
+        public string Name => "ModernSkin";
+
+        public void ApplySkin()
+        {
+            // TODO: Implement osu!lazer (modern) skin support here
+        }
+    }
+}
